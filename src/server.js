90afca4bf244dcc4631e39fb192797b712d29e3b
nitish-murugan <nitishmurugan2006@gmail.com>
2025-10-06 19:44:37 +0530

    changes after deployment - CORS

--- src/server.js
+++ src/server.js
@@ -26,7 +26,10 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://uidproject-frontend.onrender.com', 'http://localhost:3000'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
